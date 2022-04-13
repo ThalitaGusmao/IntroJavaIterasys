4d4fa6947f3d784a6016e44cee6894c0ef768f75
ThalitaGusmao <thalitagusmaosilveira@gmail.com>
2022-04-13 13:16:45 -0300

    Intro Java

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -5,17 +5,89 @@ package intro;
 
 //3 - Classe
 
+import java.util.Scanner;
+
 public class Medidas {
-    // 2.1 - Atributos - Características
+    // 3.1 - Atributos - Características
 
-    // 2.2 - Métodos e Funções
+    // 3.2 - Métodos e Funções
     public static void main(String[] args){
-        System.out.println("Cálculo de áreas!!!");
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
 
+        // Utilizar a classe Scanner do Java para ler a escolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U D E O P Ç Õ E S");
+        System.out.println("c - Calcular Area Modo Extenso");
+        System.out.println("d - Contar Até Dez");
+        System.out.println("e - Calcular Area Modo Compacto");
+        System.out.println("i - If Simples");
+        System.out.println("r - Contagem regressiva de 10 a 0 ");
+        System.out.println("s - contagem Numero");
+        System.out.println("Digite a opção desejada: ");
+        String opcao = scanner.next();
+
+        // switch = Selecionar o comportamento do programa conforme a escolha da pessoa ou do software
+
+        //String opcao = "Compacto";
+
+        switch (opcao){
+            case "c":
+            case "C":
+                System.out.println("Você escolheu executar o método CalcularAreaModoExtenso");
+                calcularAreaModoExtenso();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolheu executar o método contarAte10");
+                contarAte10();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolheu executar o método calcularAreaModoCompacto");
+                calcularAreaModoCompacto();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Você escolheu executar o método contagemRegressiva");
+                contagemRegressiva();
+                break;
+            case "S":
+            case "s":
+                System.out.println("Você escolheu executar o método contagemNumero");
+                contagemNumero();
+                break;
+            default:
+                System.out.println("Você escolheu outro valor que não tem uma ação associada ");
+                break;
+        }
+    }
+
+    public static void ifSimples(){
+
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
+
+        // if = se
+        // else = sendo
+
+        String modo = "compacto";
+
+        if (modo == "compacto"){
+            calcularAreaModoCompacto();
+        }
+        else {
+            calcularAreaModoExtenso();
+        }
 
     }
 
-    public void calcularAreaModoExtenso(){
+    public static void calcularAreaModoExtenso(){
+        System.out.println("Cálculo de áreas Modo Extenso!!!");
+
         // Calculo de área - Exemplo: o tamanho do tapete ou do piso
         int largura;
         int comprimento;
@@ -30,7 +102,9 @@ public class Medidas {
 
     }
 
-    public void calcularAreaModoCompacto(){
+    public static void calcularAreaModoCompacto(){
+        System.out.println("Cálculo de áreas Modo Compacto!!!");
+
         // Cálculo de área - Reduzido
         int largura = 4;
         int comprimento = 3;
@@ -38,4 +112,31 @@ public class Medidas {
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m A área é de " + largura * comprimento + "m²");
     }
+public static void contarAte10(){
+
+        // Loops ou Repetições
+       //  for = repetição incondicional
+    System.out.println("Contagem Crescente");
+
+       for(int numero = 1; numero <= 10; numero++){
+           System.out.println(numero);
+
+       }
+}
+
+public static void contagemRegressiva(){
+        System.out.println("Contagem Regressiva");
+
+        for(int numero = 10; numero > -1; numero--){
+            System.out.println(numero);
+        }
+}
+
+public static void contagemNumero() {
+    System.out.println("Contagem Numero");
+
+    for (int numero = 1; numero <= 5; numero++) {
+        System.out.print(numero);
+    }
+}
 }
